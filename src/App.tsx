f56bf24e1dc05367e9e9ff57ea310afdabc9918f
solo-bog <bsoloxa@gmail.com>
2021-05-12 23:49:11 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { TextField } from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <form noValidate autoComplete="off">
+        <TextField id="standard-basic" label="Standard" />
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </form>
   );
 }
 
